fef3dc46f0d85310d922b0e503164976ddf75b30
tarorow shenqi <lshenqi@gmail.com>
2014-01-27 13:27:24 +0900

    Added NNDD.app

---
+++ Casks/nndd.rb
@@ -0,0 +1,7 @@
+class Nndd < Cask
+  url 'http://jaist.dl.sourceforge.jp/nndd/59656/NNDD_v2_4_1.dmg'
+  homepage 'http://sourceforge.jp/projects/nndd/'
+  version '2.4.1'
+  md5 'd4607769bfe9a00f56ec2639cc122d25'
+  link 'Install NNDD.app'
+end
