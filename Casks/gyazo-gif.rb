d17591cc6696b20a3d581fa3786facc50f72cbfb
tarorow shenqi <lshenqi@gmail.com>
2014-01-27 13:27:44 +0900

    Added Gyazo.app

---
+++ Casks/gyazo-gif.rb
@@ -0,0 +1,7 @@
+class GyazoGif < Cask
+  url 'https://files.gyazo.com/setup/Gyazo_2.0.dmg'
+  homepage 'https://gyazo.com/'
+  version '2.0'
+  sha1 '3754ffb3fc9f592cb2648b26a4cd6ce8d9e25853'
+  link 'Gyazo Gif.app'
+end
