55f674de830471786c90a9ab2ef715f94376287f
tarorow shenqi <lshenqi@gmail.com>
2014-02-07 12:07:38 +0900

    Added Firefox with Japanese locale

---
+++ Casks/firefox-jp.rb
@@ -0,0 +1,7 @@
+class FirefoxJp < Cask
+  url 'http://download.mozilla.org/?product=firefox-latest&os=osx&lang=ja-JP-mac'
+  homepage 'https://www.mozilla.jp/firefox/'
+  version 'latest'
+  no_checksum
+  link 'Firefox.app'
+end
