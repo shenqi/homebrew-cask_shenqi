14c61d93409fc32a4ed4893f8891bd3f2e64f8e5
tarorow shenqi <lshenqi@gmail.com>
2014-03-10 20:31:40 +0900

    Use 'latest' for vuescan

--- Casks/vuescan.rb
+++ Casks/vuescan.rb
@@ -1,7 +1,7 @@
 class Vuescan < Cask
   url 'http://www.hamrick.com/files/vuex6494.dmg'
   homepage 'http://www.hamrick.com/'
-  version '9.4.23'
-  sha1 '57853d12f66f5ea1ef33cbbc86455dd90d84537a'
+  version 'latest'
+  no_checksum
   link 'VueScan.app'
 end
